3d3dd99131e7e3080b1b537661ebeb60df08b17d
Wellington Rangel <welli.rangell@gmail.com>
2025-07-02 01:51:46 -0300

    feat: define interface SupplierService

--- backend/src/main/java/com/neostore/suppliers/service/SupplierService.java
+++ backend/src/main/java/com/neostore/suppliers/service/SupplierService.java
@@ -1,4 +1,14 @@
 package com.neostore.suppliers.service;
 
+import com.neostore.suppliers.dto.SupplierDTO;
+import jakarta.validation.Valid;
+import java.util.List;
+
 public interface SupplierService {
+    SupplierDTO create(@Valid SupplierDTO dto);
+    SupplierDTO update(Long id, @Valid SupplierDTO dto);
+    void delete(Long id);
+    SupplierDTO findById(Long id);
+    List<SupplierDTO> findAll(int page, int pageSize);
+    long count();
 }
