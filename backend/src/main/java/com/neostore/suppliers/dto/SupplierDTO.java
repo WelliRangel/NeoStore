e54995fe2d38c7cc74a1271135a5b31dd152ba1c
Wellington Rangel <welli.rangell@gmail.com>
2025-07-02 01:51:46 -0300

    feat: adiciona DTO para Supplier

--- backend/src/main/java/com/neostore/suppliers/dto/SupplierDTO.java
+++ backend/src/main/java/com/neostore/suppliers/dto/SupplierDTO.java
@@ -1,4 +1,27 @@
 package com.neostore.suppliers.dto;
 
-public class SupplierDTO {
-}
+import com.neostore.suppliers.validation.Cnpj;
+import jakarta.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "id", "cnpj", "description", "email", "name" })
+public record SupplierDTO(
+        Long id,
+
+        @NotBlank
+        @Size(max = 100)
+        String name,
+
+        @NotBlank
+        @Email
+        @Size(max = 100)
+        String email,
+
+        @NotBlank
+        @Size(max = 255)
+        String description,
+
+        @NotBlank
+        @Cnpj
+        String cnpj
+) { }
