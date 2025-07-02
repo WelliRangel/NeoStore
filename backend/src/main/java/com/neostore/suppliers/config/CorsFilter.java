588978114584e9991dd9e121d212c2c42326f847
Wellington Rangel <welli.rangell@gmail.com>
2025-07-02 01:51:45 -0300

    feat: implementa filtro CORS para requisições HTTP

--- backend/src/main/java/com/neostore/suppliers/config/CorsFilter.java
+++ backend/src/main/java/com/neostore/suppliers/config/CorsFilter.java
@@ -1,4 +1,25 @@
 package com.neostore.suppliers.config;
 
-public class CorsFilter {
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+        String origin = requestContext.getHeaderString("Origin");
+        // Permite qualquer origem em desenvolvimento, mas restringe em produção!
+        if (origin != null && !origin.isEmpty()) {
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", origin);
+        } else {
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        }
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+    }
 }
