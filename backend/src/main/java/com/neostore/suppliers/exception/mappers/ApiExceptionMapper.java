5853dfd1b37c2e623f5285874d7fea048e358a2a
Wellington Rangel <welli.rangell@gmail.com>
2025-07-02 01:51:48 -0300

    feat: implementa mapeador de exceções para API

--- backend/src/main/java/com/neostore/suppliers/exception/mappers/ApiExceptionMapper.java
+++ backend/src/main/java/com/neostore/suppliers/exception/mappers/ApiExceptionMapper.java
@@ -1,4 +1,29 @@
 package com.neostore.suppliers.exception.mappers;
 
-public class ApiExceptionMapper {
+import com.neostore.suppliers.api.payload.ApiErrorResponse;
+import com.neostore.suppliers.exception.ApiException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import org.jboss.logging.Logger;
+
+@Provider
+public class ApiExceptionMapper implements ExceptionMapper<ApiException> {
+    private static final Logger LOG = Logger.getLogger(ApiExceptionMapper.class);
+
+    @Override
+    public Response toResponse(ApiException exception) {
+        LOG.error("API Exception: ", exception);
+        ApiErrorResponse error = new ApiErrorResponse(
+                "Bad Request",
+                exception.getMessage(),
+                Response.Status.BAD_REQUEST.getStatusCode(),
+                null
+        );
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
 }
