55e3c337ffc5511bc079d5cc64ef0700ffbe5f49
Wellington Rangel <welli.rangell@gmail.com>
2025-07-02 01:51:45 -0300

    feat: adiciona configuração principal da aplicação

---
+++ backend/src/main/java/com/neostore/suppliers/config/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.neostore.suppliers.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+}
